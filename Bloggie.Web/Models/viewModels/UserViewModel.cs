bd6d5a732898648ec426f2979fb5478291d0097e
sodizdaroglu <americanlaz@gmail.com>
2023-10-05 15:18:09 -0400

    CreateNew user and delete

--- Bloggie.Web/Models/viewModels/UserViewModel.cs
+++ Bloggie.Web/Models/viewModels/UserViewModel.cs
@@ -4,6 +4,13 @@
     {
         public List<User> Users{ get; set; }
 
-    
+        public string Username { get; set; }
+        
+        public string Email { get; set; }
+        
+        public string Password { get; set; }
+        
+        public bool AdminRoleCheckbox { get; set; }
+        
     }
 }
